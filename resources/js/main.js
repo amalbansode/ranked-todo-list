84d77f418850d9f406ab51974047fa0ef79ce6e4
Amal <52236912+amalbansode@users.noreply.github.com>
2020-03-28 20:53:12 -0400

    create new list keys for this app

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,8 +1,17 @@
 
 var data = (localStorage.getItem('todoList')) ? JSON.parse(localStorage.getItem('todoList')):{
-  todo: [],
-  completed: []
+  title: [],
+  due: [],
+  progress: [],
+  difficulty: []
 };
+
+//var data = {
+//  title: [],
+//  due: [],
+//  progress: [],
+//  difficulty: []
+//};
 
 // Remove and complete icons in SVG format
 var removeSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect class="noFill" width="22" height="22"/><g><g><path class="fill" d="M16.1,3.6h-1.9V3.3c0-1.3-1-2.3-2.3-2.3h-1.7C8.9,1,7.8,2,7.8,3.3v0.2H5.9c-1.3,0-2.3,1-2.3,2.3v1.3c0,0.5,0.4,0.9,0.9,1v10.5c0,1.3,1,2.3,2.3,2.3h8.5c1.3,0,2.3-1,2.3-2.3V8.2c0.5-0.1,0.9-0.5,0.9-1V5.9C18.4,4.6,17.4,3.6,16.1,3.6z M9.1,3.3c0-0.6,0.5-1.1,1.1-1.1h1.7c0.6,0,1.1,0.5,1.1,1.1v0.2H9.1V3.3z M16.3,18.7c0,0.6-0.5,1.1-1.1,1.1H6.7c-0.6,0-1.1-0.5-1.1-1.1V8.2h10.6V18.7z M17.2,7H4.8V5.9c0-0.6,0.5-1.1,1.1-1.1h10.2c0.6,0,1.1,0.5,1.1,1.1V7z"/></g><g><g><path class="fill" d="M11,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6s0.6,0.3,0.6,0.6v6.8C11.6,17.7,11.4,18,11,18z"/></g><g><path class="fill" d="M8,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C8.7,17.7,8.4,18,8,18z"/></g><g><path class="fill" d="M14,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C14.6,17.7,14.3,18,14,18z"/></g></g></g></svg>';
@@ -13,38 +22,33 @@ renderTodoList();
 // User clicked on the add button
 // If there is any text inside the item field, add that text to the todo list
 document.getElementById('add').addEventListener('click', function() {
-  var value = document.getElementById('item').value;
+  var value = document.getElementById('title').value;
   if (value) {
-    addItem(value);
+    addItem(value, 0, 0, 0);
   }
 });
 
-document.getElementById('item').addEventListener('keydown', function (e) {
+document.getElementById('title').addEventListener('keydown', function (e) {
   var value = this.value;
   if ((e.code === 'Enter' || e.code === 'NumpadEnter') && value) {
-    addItem(value);
+    addItem(value, 0, 0, 0);
   }
 });
 
-function addItem (value) {
-  addItemToDOM(value);
-  document.getElementById('item').value = '';
+function addItem (title, due, progress, difficulty) {
+  addItemToDOM(title);
+  document.getElementById('title').value = '';
 
-  data.todo.push(value);
+  data.title.push(title);
   dataObjectUpdated();
 }
 
 function renderTodoList() {
-  if (!data.todo.length && !data.completed.length) return;
+  if (!data.title.length) return;
 
-  for (var i = 0; i < data.todo.length; i++) {
-    var value = data.todo[i];
+  for (var i = 0; i < data.title.length; i++) {
+    var value = data.title[i];
     addItemToDOM(value);
-  }
-
-  for (var j = 0; j < data.completed.length; j++) {
-    var value = data.completed[j];
-    addItemToDOM(value, true);
   }
 }
 
@@ -59,9 +63,9 @@ function removeItem() {
   var value = item.innerText;
 
   if (id === 'todo') {
-    data.todo.splice(data.todo.indexOf(value), 1);
+    data.title.splice(data.title.indexOf(value), 1);
   } else {
-    data.completed.splice(data.completed.indexOf(value), 1);
+    data.title.splice(data.title.indexOf(value), 1);
   }
   dataObjectUpdated();
 
@@ -75,11 +79,11 @@ function completeItem() {
   var value = item.innerText;
 
   if (id === 'todo') {
-    data.todo.splice(data.todo.indexOf(value), 1);
-    data.completed.push(value);
+    data.todo.splice(data.title.indexOf(value), 1);
+    data.title.push(value);
   } else {
-    data.completed.splice(data.completed.indexOf(value), 1);
-    data.todo.push(value);
+    data.completed.splice(data.title.indexOf(value), 1);
+    data.title.push(value);
   }
   dataObjectUpdated();
 
